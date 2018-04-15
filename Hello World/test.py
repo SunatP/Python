303c0c68034762cd2ad200b094966872787a9d89
IZIMI\Sunat.P <galaxysuperplus@gmail.com>
2018-04-15 22:54:37 +0700

    Python is very hard

---
+++ Hello World/test.py
@@ -0,0 +1,12 @@
+has_a = "S" in "S"
+has_ahoy = "Sunat" in "Sunat" # use in to look in string
+print(has_a)    # True
+print(has_ahoy) # True
+
+phrase = """   
+Python is very hard
+"""
+length = len(phrase) # use """ to describe long text
+a = "Length is "
+word = str(length) # str is convert number to string
+print(a+ word)   # 24
