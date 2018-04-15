c022eaf52e58fb7404ebba0f917a2eb818778816
IZIMI\Sunat.P <galaxysuperplus@gmail.com>
2018-04-15 23:19:09 +0700

    update

--- Hello World/Hello world.py
+++ Hello World/Hello world.py
@@ -1,1 +1,13 @@
 print("Hello World")
+print(2+2)
+name = 'chawna'
+test = name +' '+ "Hi"
+print(test)
+print(name[3])
+print(name[0:3])
+a = b = pow(2,5)
+print(a+b)
+a =  5
+a += 5
+b =  10
+print(a==b)
--- Hello World/test.py
+++ Hello World/test.py
@@ -1,5 +1,5 @@
 has_a = "S" in "S"
-has_ahoy = "Sunat" in "Sunat" # use in to look in string
+has_ahoy = "Sunat" in "Sunat" # use in to look in string or check
 print(has_a)    # True
 print(has_ahoy) # True
 
@@ -10,3 +10,45 @@ length = len(phrase) # use """ to describe long text
 a = "Length is "
 word = str(length) # str is convert number to string
 print(a+ word)   # 24
+
+Name = "Sunat"
+Age = 19
+print("it's me %s" %Name)
+print("I'm %d Years old" % Age)
+
+car = ['Porsche' , 'Toyota' ,'Nissan']
+print(car)
+car.append('Subaru') # append is add function
+print(car)
+print(car[2])
+
+animal  = ('snp','chawna','chawbaan')
+# animal.__add__(2)
+stringf = "tuple cannot edit"
+print(stringf)
+print(animal)
+
+phone_book = {"Sunat": 832488165 ,"Somchai":12312}
+print(phone_book["Sunat"])
+
+if (Age != 19):
+    {print("Sawasdee")}
+else :{print("error")}
+
+#elif instead else if
+for i in range(5): print(i) # count loop by using range
+cheese = {2,3,4,5,5,34,4,3}
+for a in cheese : print(a)
+
+square = 0
+number = 1
+print("    ")
+while number < 10:
+    square = number ** 2 # ** is square or power
+    print(square)
+    number += 1
+    break
+
+
+
+
